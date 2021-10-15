52b9afd8c902a65f514d9a8004ccd482c1dd1d9d
r3surr3cti0n <r3surr3cti0n@protonmail.com>
2021-10-15 21:19:13 +0200

    Connect to the database using PDO and display data.

---
+++ DB.php
@@ -0,0 +1,34 @@
+<?php
+
+class DB {
+    private $host = "localhost";
+    private $user = "mariadb_user";
+    private $pwd = "VBJC5YukDsh@rB";
+    private $db = "library";
+    private $con;
+
+    public function __construct()
+    {
+        // Data Source Name (DSN)
+        $dsn = "mysql:host={$this->host};dbname={$this->db}";
+        
+        // Connect
+        try {
+            $this->con = new PDO($dsn, $this->user, $this->pwd);
+            // Set attribute to enable PHP Data Object (PDO) exceptions
+            $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $err) {
+            echo "<p><b>Connection failed!</b></p>";
+            echo "<p>{$err->getMessage()}</p>";
+        } 
+    }
+
+    // Method to fetch data from the database
+    public function viewData(){
+        $query = "SELECT * from user_details LIMIT 10";
+        $stmt = $this->con->prepare($query);  // Prepare query
+        $stmt->execute();  // Execute query
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC); // Store the data
+        return $data;                              // and return it.
+    }
+}
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+    require_once 'DB.php';
+
+    $db = new DB();
+    $data = $db->viewData();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Live search in MySQL</title>
+</head>
+<style>
+    body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        /* text-align: center; */
+        padding: 2em;
+    }
+</style>
+<body>
+    
+    <form action="search.php" method="POST">
+        <h1>Live search in MySQL</h1>
+        <input class="search-box" type="text" name="" placeholder="Start typing..." required>
+    </form>
+
+    <h2>Result:</h2>
+    <table>
+        <tr>
+            <th>user_id</th>
+            <th>username</th>
+        </tr>
+        <?php
+            foreach ($data as $line) {
+                echo "<tr>";
+                echo "<td>{$line["user_id"]}</td>";
+                echo "<td>{$line["username"]}</td>";
+                echo "</tr>";
+            }
+        ?>
+    </table>
+
+    <script src="main.js"></script>
+</body>
+</html>
