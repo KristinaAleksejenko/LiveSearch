1b00dcf6fdeb1c7bbf084531af6520cfc2700002
r3surr3cti0n <r3surr3cti0n@protonmail.com>
2021-10-17 01:19:25 +0200

    footer and search-box

--- index.php
+++ index.php
@@ -11,26 +11,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
     <title>Live search in MySQL</title>
 </head>
-<style>
-    body {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        /* text-align: center; */
-        padding: 2em;
-    }
-</style>
 <body>
     
-    <form id="search-form" action="search.php" method="POST" autocomplete="off">
-        <h1>Live search in MySQL</h1>
-        <input class="search-box" type="text" name="name" placeholder="Start typing..." autofocus required>
-    </form>
+    <main id="main">
+        <form id="search-form" action="search.php" method="POST" autocomplete="off">
+            <h1 class="title">Live search in MySQL</h1>
+            <div class="search-container">
+                <input class="search-box" type="text" name="name" placeholder="Start typing..." autofocus required>
+                <img class="search-img" src="search.svg" alt="search">
+            </div>
+        </form>
+        <table id="result"></table>
+    </main>
 
-    <h2>Result:</h2>
-    <table id="result"></table>
+
+    <footer>
+        <p class="footer-label">written by <a href="https://www.github.com/r3surr3cti0n">@r3surr3cti0n</p>
+    </footer>
 
     <script src="main.js"></script>
 </body>
